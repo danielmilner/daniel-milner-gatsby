b996613d889a861a80fed68da0f781a32d918ae7
Daniel Milner <dmilner@gmail.com>
2019-02-22 14:21:12 -0800

    HTTP2 changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -94,6 +94,14 @@ module.exports = {
         icon: 'src/images/daniel.jpg', // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-netlify`,
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
   ],
 }
